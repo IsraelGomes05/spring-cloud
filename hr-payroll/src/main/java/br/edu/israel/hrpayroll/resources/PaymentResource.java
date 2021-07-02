7bb06b630bf85c26f5aa21ffd0b0b466529c5811
Israel Gomes <israelgomes05@gmail.com>
2021-07-01 22:36:42 -0300

    hr-payroll mock

---
+++ hr-payroll/src/main/java/br/edu/israel/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,24 @@
+package br.edu.israel.hrpayroll.resources;
+
+import br.edu.israel.hrpayroll.entities.Payment;
+import br.edu.israel.hrpayroll.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        var payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+}
