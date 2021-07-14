5569db648889b51abc8bad68857574d166d9e100
Israel Gomes <israelgomes05@gmail.com>
2021-07-13 22:57:17 -0300

    create project gateway-zuul

--- hr-payroll/src/main/java/br/edu/israel/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/edu/israel/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.edu.israel.hrpayroll.resources;
 
 import br.edu.israel.hrpayroll.entities.Payment;
 import br.edu.israel.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         var payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        var payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
